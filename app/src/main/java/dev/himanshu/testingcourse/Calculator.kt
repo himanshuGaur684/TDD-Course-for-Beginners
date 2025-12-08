de4639994d49f9db910a4a40b2f9ea7e0ae5fa48
Himanshu <himanshugaur684@gmail.com>
2025-12-08 19:15:39 +0530

    AAA

---
+++ app/src/main/java/dev/himanshu/testingcourse/Calculator.kt
@@ -0,0 +1,14 @@
+package dev.himanshu.testingcourse
+
+class Calculator {
+
+    fun add(a: Int, b: Int): Int {
+        return a.plus(b)
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a.minus(b)
+    }
+
+
+}
---
+++ app/src/test/java/dev/himanshu/testingcourse/CalculatorUnitTests.kt
@@ -0,0 +1,47 @@
+package dev.himanshu.testingcourse
+
+import org.junit.Assert
+import org.junit.Test
+
+class CalculatorUnitTests {
+
+    @Test
+    fun whenTwoPositiveNumbersAdded_shouldReturnCorrectResult() {
+        // Arrange
+        val calculator = Calculator()
+
+        // Act
+        val result = calculator.add(1, 1)
+
+        // Assert
+        Assert.assertEquals(2, result)
+
+    }
+
+    // Given When Then
+    @Test
+    fun givenTwoPositiveIntegers_whenAdded_shouldReturnCorrectResult() {
+        // Arrange
+        val calculator = Calculator()
+
+        // Act
+        val result = calculator.add(1, 1)
+
+        // Assert
+        Assert.assertEquals(2, result)
+    }
+
+    // descriptive way
+    @Test
+    fun `should return correct result when added two positive numbers`() {
+        // Arrange
+        val calculator = Calculator()
+
+        // Act
+        val result = calculator.add(1, 1)
+
+        // Assert
+        Assert.assertEquals(2, result)
+    }
+
+}
